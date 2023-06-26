a8ab56ed61b53b5c0622073cd388a191b6bb8365
ahpark <from.ahpark@gmail.com>
2023-06-27 00:29:48 +0900

    퀴즈 어플리케이션

---
+++ src/main/java/com/quiz/apps/AppsApplication.java
@@ -0,0 +1,13 @@
+package com.quiz.apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppsApplication.class, args);
+	}
+
+}
